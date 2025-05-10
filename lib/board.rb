abd93e2fe4168dead51df6556ee5615d93bce854
Brittany Halterman <halterma@gmail.com>
2025-05-10 11:14:39 -0400

    Add #print_board method to display empty grid

--- lib/board.rb
+++ lib/board.rb
@@ -4,4 +4,11 @@ class Board
   def initialize
     @grid = Array.new(6) { Array.new(7) }
   end
+
+  def print_board
+    @grid.each do |row|
+      puts "|" + row.map { |cell| cell.nil? ? " " : cell }.join("|") + "|"
+    end
+    puts " 0 1 2 3 4 5 6"
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe Board do
       expect(board.grid.all? { |row| row.length == 7}).to be true
     end
   end
+
+  describe "#print_board" do
+    it "prints a 6x7 empty grid to the terminal" do
+      board = Board.new
+
+      expected_output =
+        "| | | | | | | |\n" * 6 +
+        " 0 1 2 3 4 5 6\n"
+
+      expect { board.print_board }.to output(expected_output).to_stdout
+    end
+  end
 end
