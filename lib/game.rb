249199fee557b10d3981a2b7cbc992ecb8c23bef
Brittany Halterman <halterma@gmail.com>
2025-05-10 16:34:18 -0400

    Implement Game#initialize to set up board and players

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,12 @@
 require_relative '../lib/board'
 
 class Game
-
+  def initialize
+    @board = Board.new
+    @players = [
+      { name: "Player 1", symbol: "🔴" },
+      { name: "Player 2", symbol: "🟡" }
+    ]
+    @current_player_index = 0
+  end
 end
